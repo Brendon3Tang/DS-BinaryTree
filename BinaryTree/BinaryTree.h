ce41d30a52ca7b8e302134ffad39f2728e734058
Long Tang <kyle3tang@gmail.com>
2021-11-25 23:07:06 +0800

    adjust some formats

--- BinaryTree/BinaryTree.h
+++ BinaryTree/BinaryTree.h
@@ -23,8 +23,7 @@ public:
 
 	BinaryTree() { this->root = Create(root); depth = 0; leavesNum = 0; saveRoot = NULL; leftOrRight = 0; }
 
-	~BinaryTree()
-	{ Release(root); cout << "析构完成" << endl;}
+	~BinaryTree() { Release(root); cout << "析构完成" << endl;}
 
 	void PreOrder() { PreOrder(root); }
 
